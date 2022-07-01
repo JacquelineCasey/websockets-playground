102f1e004e2f6a862a1028a038759a2281975162
jackcasey067 <jackcasey067@gmail.com>
2022-07-01 17:19:48 -0500

    Color and :quit update

--- common.py
+++ common.py
@@ -2,16 +2,28 @@
 import aioconsole # Async Input library
 
 
+class QuitException(Exception):
+    pass
+
+BRIGHT_GREEN = '\u001b[32;1m'
+BRIGHT_BLUE = '\u001b[34;1m'
+BRIGHT_RED = '\u001b[31;1m'
+RESET_COLOR = '\u001b[0m'
+SHUTDOWN_CODE = f'{BRIGHT_RED}Other User Left the Chat{RESET_COLOR}' # in red
+
+
 async def print_incoming_messages(websocket):
     async for msg in websocket:
-        print(f'[Other] {msg}')
-
+        if msg == SHUTDOWN_CODE:
+            print(SHUTDOWN_CODE)
+            raise QuitException("Other ran :quit.")
+        print(f'{BRIGHT_GREEN}[Other]{RESET_COLOR} {msg}')
 
 async def send_input(websocket):
     while True:
         msg = await aioconsole.ainput()
-        # if (msg == ':quit'):
-        #     print('=== Exiting Program ===')
-        #     exit(0)
-        print (f'\033[1A[You] {msg}\033[K') # Control Sequence. Replaces the printed input line.
+        if (msg == ':quit'):
+            await websocket.send(SHUTDOWN_CODE)
+            raise QuitException("User ran :quit.")
+        print (f'\033[1A{BRIGHT_BLUE}[You]{RESET_COLOR} {msg}\033[K') # Control Sequence replaces the printed input line.
         await websocket.send(msg)
--- console.py
+++ console.py
@@ -3,31 +3,35 @@ import asyncio
 import sys
 import websockets
 
-from common import print_incoming_messages, send_input
+from common import *
 
 
 HOST = 'jacks-macbook-pro.local' # Could be 'localhost'
 
 
-async def send_and_receive_messages(websocket):
+async def send_and_receive_messages(websocket, keep_alive = None):
     try:
         print('=== Connected. ===')
-        await asyncio.gather( # Do multiple things at once.
+        print(f'{BRIGHT_RED}Note:{RESET_COLOR} End the session by typing \":quit\"')
+        await asyncio.gather( # Do multiple things at once. Any error is raised immediately
             print_incoming_messages(websocket), 
-            send_input(websocket)
+            send_input(websocket),
         )
+    except QuitException:
+        print('=== Exiting Program ===')
     except Exception as e:
         print("=== Something went wrong! ===")
         print(e.with_traceback())
         print("=== End Error Message ===")
-
+    finally: # Quit the server after first websocket dies for any reason.
+        if keep_alive:
+            keep_alive.set_result(None)
 
 def help():
     print('Unrecognized or missing command line argument: ')
     print('Use send mode with "--send" or "-s", and use receive mode with "--receive" or "-r".')
     print('Ex: python3.10 console.py s')
     exit(1)
-
 
 async def main():
     if len(sys.argv) < 2:
@@ -36,8 +40,10 @@ async def main():
     match sys.argv[1] or "none":
         case '--receive' | '-r': 
             print("=== Awaiting Connection... ===")
-            async with websockets.serve(send_and_receive_messages, '', 8001): # On local machine, navigating to http://jacks-macbook-pro.local:8001 does something interesting.
-                await asyncio.Future()  # run forever
+            keep_alive = asyncio.Future()
+
+            async with websockets.serve(lambda ws: send_and_receive_messages(ws, keep_alive), '', 8001): # On local machine, navigating to http://jacks-macbook-pro.local:8001 does something interesting.
+                await keep_alive  # run until keep_alive.set_result()
         case '--send' | '-s':
             print(f'=== Attempting to Connect to {HOST}... ===')
             async with websockets.connect(f'ws://{HOST}:8001/') as websocket:
